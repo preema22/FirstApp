234317754080bd7eb66b62fc4eec777b658ff990
Preema DSouza <preema22@gmail.com>
2022-05-16 15:15:14 +0200

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Preema DSouza on 16/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
